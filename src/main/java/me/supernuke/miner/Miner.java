ad4d7c6afe1465016b2851e8b8937447113dcd4c
BuildTools <unconfigured@null.spigotmc.org>
2018-01-29 21:19:58 -0500

    Initial commit

---
+++ src/main/java/me/supernuke/miner/Miner.java
@@ -0,0 +1,17 @@
+package me.supernuke.miner;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Miner extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
